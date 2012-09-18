360b21a776bb6a3cbd68690b197d62a2a7cb12fb
minecraftshipwreck@gmail.com <Iain@Iain-PC>
2012-09-18 17:43:07 +0100

    test

---
+++ src/com/bitzcraftonline/bitzifacts/Bitzifacts.java
@@ -0,0 +1,24 @@
+package com.bitzcraftonline.bitzifacts;
+
+import java.util.logging.Logger;
+import net.milkbowl.vault.economy.*;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Bitzifacts extends JavaPlugin {
+	
+	public static Bitzifacts plugin;
+	public final Logger logger = Logger.getLogger("Minecraft");
+
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+	
+}
